ee079d697d3102be7bff32c6129a3d776de2371c
Alvin666 <jian0088@algonquinlive.com>
2021-06-19 17:55:23 -0400

    Initialized project by deleting unused files and add components folder with header, resource and show cat part

---
+++ src/Components/Resource.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Resource = ({path, }) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Resource
---
+++ src/Components/ShowCat.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const ShowCat = () => {
+    return (
+        <div>
+            <h1>show</h1>
+        </div>
+    )
+}
+
+export default ShowCat
