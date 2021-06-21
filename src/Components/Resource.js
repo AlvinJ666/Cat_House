b1fa5d1640e3d968aeaf9c7edaa0fb449576e80e
Alvin666 <jian0088@algonquinlive.com>
2021-06-20 22:25:19 -0400

    Base structure finished, initial data loading

--- src/Components/Resource.js
+++ src/Components/Resource.js
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios';
 
+const Resource = ({path,render }) => {
+    const initialState={
+        trans:[],
+        loading:true,
+        error:null
+    }//object structure, extensible
+    const [state, setState]=useState(initialState);
 
-const Resource = ({path, }) => {
+    const getData= async (path) =>{
+        try {
+            const result = await axios.get(path);   //axios to request and get data
+            console.log('result: ', result);
+            const newData = {
+                trans = result.data,
+                loading:false,
+                error:null,
+            }
+            setState(newData);
+        } catch (error) {
+            console.log('error: ', error);
+        }
+       
+
+    }
+
     return (
+
+
         <div>
-            
+            Resource
+            {render(state)}
         </div>
     )
 }
--- src/Components/ShowCat.js
+++ src/Components/ShowCat.js
@@ -1,12 +1,21 @@
 import React from 'react';
-
+import Resource from './Resource';
 
 const ShowCat = () => {
+    const webURL = 'https://dog.ceo/api/breeds/image/random/50';
+    
+    const render =  (data)=>{
+        if(data.loading===false)//if not received data
+            return null;
+
+        console.log();
+    }
     return (
         <div>
             <h1>show</h1>
+            <Resource path={webURL}/>
         </div>
     )
 }
 
-export default ShowCat
+export default ShowCat;
